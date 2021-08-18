700c05fa1c578fc9215ea0de5ef8626f5c868a21
romanTechn <rom.okspb@gmail.com>
2021-08-18 20:18:25 +0300

    Add counter

---
+++ src/components/counter.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const formCount = () => {
+    return count === 0 ? "Ноль" : count;
+  };
+
+  const getBadgeClasses = () => {
+    let classes = "badge m-2 bg-";
+    classes += count === 0 ? "danger" : "primary";
+    return classes;
+  };
+
+  const handleIncrement = (productId) => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = (productId) => {
+    setCount(count === 0 ? count : count - 1);
+  };
+
+  return (
+    <>
+      <span className={getBadgeClasses()}>{formCount()}</span>
+      <button
+        onClick={() => handleIncrement({ id: 1 })}
+        className="btn btn-secondary btn-sm"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => handleDecrement({ id: 1 })}
+        className="btn btn-secondary btn-sm"
+      >
+        Decrement
+      </button>
+    </>
+  );
+};
+
+export default Counter;
